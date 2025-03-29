e3df7e87e99df89da3630e3b1d7e55e220691c71
Dan Sosedoff <dan.sosedoff@gmail.com>
2025-03-29 13:28:49 -0700

    Add sidekiq ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   root "locations#index"
 
@@ -8,4 +10,9 @@ Rails.application.routes.draw do
   # Current weather and forecasting pages
   get "weather/:id", to: "forecast#current", as: "current_weather"
   get "weather/:id/extended", to: "forecast#extended", as: "extended_weather"
+
+  # Sidekiq UI for inspecting jobs
+  if Rails.env.development?
+    mount Sidekiq::Web => "/_sidekiq"
+  end
 end
